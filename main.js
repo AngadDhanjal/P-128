98b430e106fb15cb93cb11928de0b1b4cbc3838d
AngadDhanjal <88576513+AngadDhanjal@users.noreply.github.com>
2022-10-09 13:50:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 Peter_pan="";
 Harry_potter="";
-rightWristX = 0;
+rightWristx = 0;
 rightWristY = 0;
 leftWristX = 0;
 leftWristY = 0;
@@ -13,11 +13,11 @@ function setup(){
     video.hide();
 
     poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose,gotposes');
+    poseNet.on('pose',gotposes);
 }
 
 function preload(){
-    Peter_pan= loadSound("music2.mp3");
+    Peter_pan = loadSound("music2.mp3");
     Harry_potter = loadSound("music.mp3");
 }
 
@@ -25,23 +25,20 @@ function draw(){
     image(video,0,0,600,530);
 }
 
-function modelLoaded()
-{
-    console.log("PoseNet Is Initialized");
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
 }
 
-function gotposes(results)
-{
-    if(results.length > 0)
-    {
+function gotposes(results){
+    if(results.length > 0){
         console.log(results);
+         
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " +leftWristX+"leftWristY = "+leftWristY);
+        console.log("leftWristX = "+leftWristX+" leftWristY = "+leftWristY);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
-
+        console.log("rightWristX = "+rightWristX+" rightWristY = "+rightWristY);
     }
 }
